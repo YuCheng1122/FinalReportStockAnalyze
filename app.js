fb211e5c89057bb1439ed558669d019080901c52
YuCheng1122 <tommymask00@gmail.com>
2023-08-13 15:10:06 +0800

    rmjson

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const db = require("./database/mysql");
 
-db.connect();
+// ... 其他代碼
 
-// 使用 db 進行資料庫操作...
-
-// 在適當的時候
-db.end();
+// 不需要 db.connect() 和 db.end()
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
