6ee2f21f34128ed1ee08ab6882f92e7de135b3e3
Yucheng1122 <tommymask00@gmail>
2023-12-18 16:05:40 +0800

    queryForstock

--- src/models/investmentTech.js
+++ src/models/investmentTech.js
@@ -6,30 +6,34 @@ const { AppError } = require('../config/error_classes')
 const getFinancialData = () => {
   return new Promise((resolve, reject) => {
     const sql = `
-        SELECT 
-          b.stock_id, 
-          s.name, 
-          b.ROE, 
-          sd_latest.closing_price,
-          i.eps,
-          i.epsYoY,
-          fs.link
-        FROM 
-          balancesheetliabilitiesequity b
-        JOIN 
-          stock s ON b.stock_id = s.stock_id
-        JOIN 
-          (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
-        JOIN 
-          (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
-        JOIN 
-          (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3) i ON b.stock_id = i.stock_id
-        JOIN 
-          (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
-        WHERE 
-          CAST(b.debtRatio AS DECIMAL(10, 2)) < 30 
-          AND b.year = 2023
-          AND b.quarter = 3;
+    SELECT 
+    b.stock_id, 
+    s.name, 
+    b.ROE, 
+    sd_latest.closing_price,
+    i.eps,
+    i.epsYoY,
+    fs.link
+FROM 
+    balancesheetliabilitiesequity b
+JOIN 
+    stock s ON b.stock_id = s.stock_id
+JOIN 
+    (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
+JOIN 
+    (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
+JOIN 
+    (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3) i ON b.stock_id = i.stock_id
+JOIN 
+    (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
+WHERE 
+    CAST(b.debtRatio AS DECIMAL(10, 2)) < 30 
+    AND b.year = 2023
+    AND b.quarter = 3
+ORDER BY 
+    CAST(i.eps AS DECIMAL(10, 2)) DESC
+LIMIT 5;
+
       `
 
     db.query(sql, (error, result) => {
@@ -45,30 +49,34 @@ const getFinancialData = () => {
 const getHighROEFinancialData = () => {
   return new Promise((resolve, reject) => {
     const sql = `
-        SELECT 
-          b.stock_id, 
-          s.name, 
-          b.ROE, 
-          sd_latest.closing_price,
-          i.eps,
-          i.epsYoY,
-          fs.link
-        FROM 
-          balancesheetliabilitiesequity b
-        JOIN 
-          stock s ON b.stock_id = s.stock_id
-        JOIN 
-          (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
-        JOIN 
-          (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
-        JOIN 
-          (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3) i ON b.stock_id = i.stock_id
-        JOIN 
-          (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
-        WHERE 
-          b.ROE > 1.5
-          AND b.year = 2023
-          AND b.quarter = 3;
+    SELECT 
+    b.stock_id, 
+    s.name, 
+    b.ROE, 
+    sd_latest.closing_price,
+    i.eps,
+    i.epsYoY,
+    fs.link
+FROM 
+    balancesheetliabilitiesequity b
+JOIN 
+    stock s ON b.stock_id = s.stock_id
+JOIN 
+    (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
+JOIN 
+    (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
+JOIN 
+    (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3) i ON b.stock_id = i.stock_id
+JOIN 
+    (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
+WHERE 
+    b.ROE > 1.5
+    AND b.year = 2023
+    AND b.quarter = 3
+ORDER BY 
+    CAST(i.eps AS DECIMAL(10, 2)) DESC
+LIMIT 5;
+
       `
 
     db.query(sql, (error, result) => {
@@ -84,7 +92,16 @@ const getHighROEFinancialData = () => {
 const getPositiveCashFlowData = () => {
   return new Promise((resolve, reject) => {
     const sql = `
-      SELECT 
+    SELECT 
+    filtered_data.stock_id, 
+    filtered_data.name, 
+    filtered_data.ROE, 
+    filtered_data.closing_price,
+    filtered_data.eps,
+    filtered_data.epsYoY,
+    filtered_data.link
+FROM (
+    SELECT 
         b.stock_id, 
         s.name, 
         b.ROE, 
@@ -92,23 +109,28 @@ const getPositiveCashFlowData = () => {
         i.eps,
         i.epsYoY,
         fs.link
-      FROM 
+    FROM 
         balancesheetliabilitiesequity b
-      JOIN 
+    JOIN 
         stock s ON b.stock_id = s.stock_id
-      JOIN 
+    JOIN 
         (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
-      JOIN 
+    JOIN 
         (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
-      JOIN 
+    JOIN 
         (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3) i ON b.stock_id = i.stock_id
-      JOIN 
+    JOIN 
         (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
-      JOIN 
-        (SELECT stock_id FROM cash_flow_statement WHERE free_cash_flow NOT LIKE '%-%' AND free_cash_flow IS NOT NULL AND free_cash_flow <> '') cfs ON b.stock_id = cfs.stock_id
-      WHERE 
+    JOIN 
+        (SELECT stock_id FROM cashflowstatement WHERE freeCashFlow NOT LIKE '%-%' AND freeCashFlow IS NOT NULL AND freeCashFlow <> '' AND YEAR=2023 AND QUARTER=3) cfs ON b.stock_id = cfs.stock_id
+    WHERE 
         b.year = 2023
-        AND b.quarter = 3;
+        AND b.quarter = 3
+) AS filtered_data
+ORDER BY 
+    CAST(filtered_data.eps AS DECIMAL(10, 2)) DESC
+LIMIT 5;
+
     `
 
     db.query(sql, (error, result) => {
@@ -124,7 +146,8 @@ const getPositiveCashFlowData = () => {
 const getFinancialDataWithCurrentRatio = () => {
   return new Promise((resolve, reject) => {
     const sql = `
-      SELECT 
+      SELECT * FROM (
+    SELECT 
         b.stock_id, 
         s.name, 
         b.ROE, 
@@ -133,25 +156,28 @@ const getFinancialDataWithCurrentRatio = () => {
         i.epsYoY,
         fs.link,
         ast.currentRatio
-      FROM 
+    FROM 
         balancesheetliabilitiesequity b
-      JOIN 
+    JOIN 
         stock s ON b.stock_id = s.stock_id
-      JOIN 
+    JOIN 
         (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
-      JOIN 
+    JOIN 
         (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
-      JOIN 
+    JOIN 
         (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3) i ON b.stock_id = i.stock_id
-      JOIN 
+    JOIN 
         (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
-      JOIN 
-        (SELECT stock_id FROM cash_flow_statement WHERE free_cash_flow NOT LIKE '%-%' AND free_cash_flow IS NOT NULL AND free_cash_flow <> '') cfs ON b.stock_id = cfs.stock_id
-      JOIN 
-        (SELECT stock_id, currentRatio FROM assetstatements WHERE currentRatio > 200) ast ON b.stock_id = ast.stock_id
-      WHERE 
+    JOIN 
+        (SELECT stock_id, currentRatio FROM assetstatements WHERE currentRatio > 200 AND YEAR=2023 AND QUARTER=3) ast ON b.stock_id = ast.stock_id
+    WHERE 
         b.year = 2023
-        AND b.quarter = 3;
+        AND b.quarter = 3
+) AS subquery
+ORDER BY 
+    CAST(subquery.eps AS DECIMAL(10, 2)) DESC
+LIMIT 5;
+
     `
 
     db.query(sql, (error, results) => {
@@ -170,31 +196,32 @@ const getFinancialDataWithCurrentRatio = () => {
 const getFinancialData2023Q3 = () => {
   return new Promise((resolve, reject) => {
     const sql = `
-        SELECT 
-          b.stock_id, 
-          s.name, 
-          b.ROE, 
-          sd_latest.closing_price,
-          i.eps,
-          i.epsYoY,
-          fs.link
-        FROM 
-          balancesheetliabilitiesequity b
-        JOIN 
-          stock s ON b.stock_id = s.stock_id
-        JOIN 
-          (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
-        JOIN 
-          (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
-        JOIN 
-          (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3 AND eps > 0) i ON b.stock_id = i.stock_id
-        JOIN 
-          (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
-        JOIN 
-          (SELECT stock_id FROM cash_flow_statement WHERE free_cash_flow NOT LIKE '%-%' AND free_cash_flow IS NOT NULL AND free_cash_flow <> '') cfs ON b.stock_id = cfs.stock_id
-        WHERE 
-          b.year = 2023
-          AND b.quarter = 3;
+    SELECT 
+    b.stock_id, 
+    s.name, 
+    b.ROE, 
+    sd_latest.closing_price,
+    i.eps,
+    i.epsYoY,
+    fs.link
+FROM 
+    balancesheetliabilitiesequity b
+JOIN 
+    stock s ON b.stock_id = s.stock_id
+JOIN 
+    (SELECT stock_id, MAX(date) AS latest_date FROM stock_day_all WHERE YEAR(date) = 2023 GROUP BY stock_id) sd_max_date ON b.stock_id = sd_max_date.stock_id
+JOIN 
+    (SELECT stock_id, closing_price, date FROM stock_day_all) sd_latest ON sd_max_date.stock_id = sd_latest.stock_id AND sd_max_date.latest_date = sd_latest.date
+JOIN 
+    (SELECT stock_id, eps, epsYoY FROM incomestatements WHERE year = 2023 AND quarter = 3 AND eps > 0) i ON b.stock_id = i.stock_id
+JOIN 
+    (SELECT stock_id, link FROM financial_statement WHERE year = 2023 AND season = 3) fs ON b.stock_id = fs.stock_id
+WHERE 
+    b.year = 2023
+    AND b.quarter = 3
+ORDER BY 
+    CAST(i.eps AS DECIMAL(10, 2)) DESC
+LIMIT 5;
       `
 
     db.query(sql, (error, results) => {
